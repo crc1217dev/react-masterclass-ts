05c9af2f42c875842beccceec74f8a34776920ab
crc1217dev <100065353+crc1217dev@users.noreply.github.com>
2023-03-13 20:47:49 +0900

    add chart and fix chart design

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -277,7 +277,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
